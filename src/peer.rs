39415a69613eafaa69c61fb28230293d1539ba64
Bastian Köcher <git@kchr.de>
2018-02-23 11:54:11 +0100

    Adds PeerBuilder

--- src/peer.rs
+++ src/peer.rs
@@ -1,12 +1,13 @@
 use error::*;
 use protocol::Protocol;
+use service::Service;
 
 use std::path::PathBuf;
 use std::rc::Rc;
 use std::cell::RefCell;
 use std::net::SocketAddr;
 
-use hole_punch::{plain, Config, Context, Stream};
+use hole_punch::{Config, Context, Stream};
 
 use futures::{Future, Poll, Sink, Stream as FStream};
 use futures::Async::Ready;
@@ -25,19 +26,19 @@ impl PeerContext {
     }
 }
 
-struct Peer {
-    handle: Handle,
+pub struct PeerBuilder {
     context: Context<Protocol>,
+    handle: Handle,
     peer_context: PeerContextPtr,
 }
 
-impl Peer {
-    pub fn new<S: Into<PathBuf>, T: Into<PathBuf>>(
+impl PeerBuilder {
+    fn new<S: Into<PathBuf>, T: Into<PathBuf>>(
         handle: &Handle,
         cert_file: S,
         key_file: T,
         name: String,
-    ) -> Result<Peer> {
+    ) -> Result<PeerBuilder> {
         let config = Config {
             udp_listen_address: ([0, 0, 0, 0], 0).into(),
             cert_file: cert_file.into(),
@@ -46,26 +47,112 @@ impl Peer {
 
         let context = Context::new(handle.clone(), config)?;
 
-        Ok(Peer {
+        Ok(PeerBuilder {
             context,
             handle: handle.clone(),
             peer_context: PeerContext::new(name),
         })
     }
 
-    pub fn register_and_run(mut self, evt_loop: &mut Core, server: &SocketAddr) -> Result<()> {
+    fn register(mut self, server: &SocketAddr) -> BuildPeer {
         let peer_context = self.peer_context.clone();
         let name = peer_context.borrow().name.clone();
-        let con = self.context
+        let future = self.context
             .create_connection_to_server(server)
             .map_err(|e| e.into())
             .and_then(move |s| InitialConnection::register(s, name))
-            .and_then(|s| Connection::new(s, peer_context));
+            .map(move |s| {
+                Peer::new(
+                    self.handle,
+                    self.context,
+                    self.peer_context,
+                    Connection::new(s, peer_context),
+                )
+            });
 
-        evt_loop.run(self.join(con)).map(|_| ()).map_err(|e| e.0)
+        BuildPeer {
+            future: Box::new(future),
+        }
     }
 
-    pub fn login() {}
+    fn login(mut self, server: &SocketAddr, pw: String) -> BuildPeer {
+        let peer_context = self.peer_context.clone();
+        let name = peer_context.borrow().name.clone();
+        let future = self.context
+            .create_connection_to_server(server)
+            .map_err(|e| e.into())
+            .and_then(move |s| InitialConnection::login(s, name, pw))
+            .map(move |s| {
+                Peer::new(
+                    self.handle,
+                    self.context,
+                    self.peer_context,
+                    Connection::new(s, peer_context),
+                )
+            });
+
+        BuildPeer {
+            future: Box::new(future),
+        }
+    }
+}
+
+struct BuildPeer {
+    future: Box<Future<Item = Peer, Error = Error>>,
+}
+
+impl Future for BuildPeer {
+    type Item = Peer;
+    type Error = Error;
+
+    fn poll(&mut self) -> Poll<Self::Item, Self::Error> {
+        self.future.poll()
+    }
+}
+
+struct Peer {
+    handle: Handle,
+    context: Context<Protocol>,
+    peer_context: PeerContextPtr,
+    server_con: Connection,
+}
+
+impl Peer {
+    fn new(
+        handle: Handle,
+        context: Context<Protocol>,
+        peer_context: PeerContextPtr,
+        server_con: Connection,
+    ) -> Peer {
+        Peer {
+            handle,
+            context,
+            peer_context,
+            server_con,
+        }
+    }
+
+    pub fn builder<S: Into<PathBuf>, T: Into<PathBuf>>(
+        handle: &Handle,
+        cert_file: S,
+        key_file: T,
+        name: String,
+    ) -> Result<PeerBuilder> {
+        PeerBuilder::new(handle, cert_file, key_file, name)
+    }
+
+    pub fn run(self, evt_loop: &mut Core) -> Result<()> {
+        evt_loop.run(self)
+    }
+
+    pub fn request_connection_and_run_service<S: Service>(
+        self,
+        evt_loop: &mut Core,
+        name: String,
+        service: S,
+    ) -> Result<()> {
+        unimplemented!()
+    }
 }
 
 impl Future for Peer {
@@ -73,9 +160,16 @@ impl Future for Peer {
     type Error = Error;
 
     fn poll(&mut self) -> Poll<Self::Item, Self::Error> {
+        if self.server_con.poll()?.is_ready() {
+            return Ok(Ready(()));
+        }
+
         loop {
             match try_ready!(self.context.poll()) {
-                Some(stream) => {}
+                Some(stream) => self.handle.spawn(
+                    Connection::new(stream, self.peer_context.clone())
+                        .map_err(|e| eprintln!("{:?}", e)),
+                ),
                 None => return Ok(Ready(())),
             }
         }
@@ -162,6 +256,7 @@ impl Future for InitialConnection {
 
             match msg {
                 Protocol::RegisterSuccessFul | Protocol::LoginSuccessful => {
+                    println!("Registration/Login successful");
                     let mut stream = self.stream.take().unwrap();
                     stream.upgrade_to_authenticated();
                     return Ok(Ready(stream));

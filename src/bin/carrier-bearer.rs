67e12659d8ce436499945b1149351e53efabc42d
Bastian Köcher <git@kchr.de>
2018-09-12 22:13:47 +0200

    Switch to `structopt` in carrier-bearer

--- src/bin/carrier-bearer.rs
+++ src/bin/carrier-bearer.rs
@@ -1,37 +1,48 @@
 extern crate carrier;
+extern crate structopt;
 extern crate tokio_core;
 
 use tokio_core::reactor::Core;
 
-use std::env::var;
+use std::path::PathBuf;
+
+use structopt::StructOpt;
+
+#[derive(StructOpt, Debug)]
+#[structopt(name = "carrier-bearer")]
+struct Options {
+    /// The path to the certificate of this bearer.
+    #[structopt(long = "certificate", parse(from_os_str))]
+    certificate: PathBuf,
+    /// The path to the private key of this bearer.
+    #[structopt(long = "private_key", parse(from_os_str))]
+    private_key: PathBuf,
+    /// The port this bearer should listen on.
+    #[structopt(long = "listen_port", default_value = "22222")]
+    listen_port: u16,
+    /// The path to trusted authorities for incoming connections in PEM format(filename: *.pem).
+    #[structopt(long = "incoming_con_ca_path", parse(from_os_str))]
+    incoming_con_ca_path: PathBuf,
+}
 
 fn main() {
-    let certificate_path =
-        var("CARRIER_CERT_PATH").expect("Please give path to cert file via `CARRIER_CERT_PATH`");
-    let key_path = var("CARRIER_KEY_PATH")
-        .expect("Please give path to private key file via `CARRIER_KEY_PATH`");
-    let listen_port = var("CARRIER_LISTEN_PORT")
-        .map(|v| v.parse())
-        .unwrap_or(Ok(22222))
-        .expect("Integer value for `CARRIER_LISTEN_PORT`");
-    let client_ca_path = var("CARRIER_CLIENT_CA_PATH").expect(
-        "Please give path to client certificate authorities(*.pem) via `CARRIER_CLIENT_CA_PATH`",
-    );
+    let options = Options::from_args();
 
-    let client_ca_vec = carrier::util::glob_for_certificates(&client_ca_path)
-        .expect("Globbing for client certificate authorities(*.pem).");
+    let incoming_con_ca_vec =
+        carrier::util::glob_for_certificates(&options.incoming_con_ca_path.display())
+            .expect("Globbing for incoming connection certificate authorities(*.pem).");
 
     let mut evt_loop = Core::new().unwrap();
 
     let builder = carrier::Peer::builder(evt_loop.handle())
-        .set_quic_listen_port(listen_port)
-        .set_certificate_chain_file(certificate_path)
-        .set_private_key_file(key_path)
-        .set_client_ca_cert_files(client_ca_vec);
+        .set_quic_listen_port(options.listen_port)
+        .set_certificate_chain_file(options.certificate)
+        .set_private_key_file(options.private_key)
+        .set_client_ca_cert_files(incoming_con_ca_vec);
 
     let builder = carrier::builtin_services::register(builder);
 
-    println!("Bearer running (Port: {})", listen_port);
+    println!("Bearer running (Port: {})", options.listen_port);
     let bearer = builder.build().unwrap();
 
     bearer.run(&mut evt_loop).unwrap();
